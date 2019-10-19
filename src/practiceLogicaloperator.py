5a1bfc5f65f6457105d40fc171c34070ac6fc4d0
ArturoITA <55923813+ArturoITA@users.noreply.github.com>
2019-10-19 11:01:23 -0500

    Initial commit

---
+++ src/practiceLogicaloperator.py
@@ -0,0 +1,16 @@
+'''
+Created on Oct 5, 2019
+
+@author: ITAUser
+'''
+True
+
+print(4 == 4 and (6 == 5 or 2 < 5) or 25 < 9)
+print(4 == 4 and (6 == 5 or 2 < 5) and 25 < 9)
+
+a = False
+b = 20 > 9
+c = 1 == 1
+d = True
+e = True
+print(a and (b and c) or (d and e))
